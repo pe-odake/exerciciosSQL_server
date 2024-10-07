be7d2c18877e5db4207e76aef81d2bc49b144d6a
Pedro Odake <165703800+pe-odake@users.noreply.github.com>
2024-10-07 14:11:19 -0300

    Update exerciciosSQL_server.sql

--- exerciciosSQL_server.sql
+++ exerciciosSQL_server.sql
@@ -1,46 +1,50 @@
---use ContosoRetailDW
+use ContosoRetailDW
 
---Quest„o 1
+--Quest√£o 1
 
---SELECT CustomerKey
---FROM DimCustomer
---Houve uma reduÁ„o do n˙mero de clientes havia um total de 19500, e agora h· 18869
+SELECT ProductKey
+FROM DimProduct
+--N√£o houve mudan√ßa no n√∫mero de items
 
---Quest„o 2
+SELECT CustomerKey
+FROM DimCustomer
+--Houve uma redu√ß√£o do n√∫mero de clientes havia um total de 19500, e agora h√° 18869
 
---SELECT
---CustomerKey AS Cliente,
---FirstName AS 'Nome',
---EmailAddress AS Email,
---BirthDate AS 'Aniversario do Cliente' 
---FROM DimCustomer
+--Quest√£o 2
 
---Quest„o 3
+SELECT
+CustomerKey AS Cliente,
+FirstName AS 'Nome',
+EmailAddress AS Email,
+BirthDate AS 'Aniversario do Cliente' 
+FROM DimCustomer
 
---SELECT TOP (100) 
---FirstName AS '1∞ Nome',
---EmailAddress AS Email,
---BirthDate AS 'Anivers·rio do Cliente'
---FROM DimCustomer
+--Quest√£o 3
 
---SELECT TOP (20) PERCENT 
---FirstName AS '1∞ Nome',
---EmailAddress AS Email,
---BirthDate AS 'Anivers·rio do Cliente'
---FROM DimCustomer
+SELECT TOP (100) 
+FirstName AS '1¬∞ Nome',
+EmailAddress AS Email,
+BirthDate AS 'Anivers√°rio do Cliente'
+FROM DimCustomer
 
---Quest„o 4
+SELECT TOP (20) PERCENT 
+FirstName AS '1¬∞ Nome',
+EmailAddress AS Email,
+BirthDate AS 'Anivers√°rio do Cliente'
+FROM DimCustomer
 
---SELECT DISTINCT
---Manufacturer AS Forcedores
---FROM DimProduct
+--Quest√£o 4
 
---Quest„o 5
+SELECT DISTINCT
+Manufacturer AS Forcedores
+FROM DimProduct
 
---SELECT DISTINCT ProductKey
---FROM DimProduct
+--Quest√£o 5
+
+SELECT DISTINCT ProductKey
+FROM DimProduct
 
 --SELECT DISTINCT ProductKey
 --FROM FactSales
 
---h· um produto que n„o foi vendido
+--h√° um produto que n√£o foi vendido
