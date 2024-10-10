750b9715a7fe444a10e3dc66ed4f993aa1f372ff
Pedro Odake <165703800+pe-odake@users.noreply.github.com>
2024-10-10 05:46:22 -0300

    Faltando resolver exercicio 10

--- Exerc04.sql
+++ Exerc04.sql
@@ -72,7 +72,7 @@ use ContosoRetailDW
 
 --SELECT 
 --	BrandName,
---	COUNT(ProductKey) AS 'Número de Produtos por Marca'
+--	COUNT(ProductKey) AS 'NÃºmero de Produtos por Marca'
 	
 --FROM DimProduct
 --GROUP BY BrandName
@@ -80,7 +80,7 @@ use ContosoRetailDW
 ----b)
 --SELECT 
 --	ClassName,
---	AVG(UnitPrice) AS 'Média por Classe'
+--	AVG(UnitPrice) AS 'MÃ©dia por Classe'
 
 --FROM DimProduct
 --GROUP BY ClassName
@@ -93,10 +93,54 @@ use ContosoRetailDW
 --FROM DimProduct
 --GROUP BY ColorName
 
---5)
+----5)
 
+--SELECT 
+--	StockTypeName,
+--	SUM(Weight)
+
+--FROM DimProduct
+--WHERE BrandName = 'Contoso'
+--GROUP BY StockTypeName
+--ORDER BY SUM(Weight) DESC
+
+----6)
+
+--SELECT 
+--	BrandName,
+--	COUNT(DISTINCT ColorName)
+
+--FROM DimProduct
+--GROUP BY BrandName
+--ORDER BY COUNT(DISTINCT ColorName) ASC
+
+----7)
+
+--SELECT 
+--	Gender,
+--	COUNT(CustomerKey),
+--	AVG(YearlyIncome)
+
+--FROM DimCustomer
+--WHERE Gender IS NOT NULL
+--GROUP BY Gender
+
+----9)
+
+--SELECT
+--	DepartmentName,
+--	COUNT(EmployeeKey)
+	
+
+--FROM DimEmployee
+--WHERE EndDate IS NULL
+--GROUP BY DepartmentName
+--ORDER BY COUNT(EmployeeKey) DESC
+
+--10)
 SELECT 
-	ProductKey,
-	StockTypeName
+	Title,
+	SUM(VacationHours)
 
-FROM DimProduct
+FROM DimEmployee
+GROUP BY Title
